4de66fb0259adac51cda1a6a4a13fe967a52325e
BenLumenDigital <53438826+BenLumenDigital@users.noreply.github.com>
2025-04-13 12:37:46 +0100

    Kokoro Swift example

---
+++ swift/Swift-TTS/Swift-TTS/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Swift-TTS
+//
+//  Created by Ben Harraway on 13/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    private let kokoroTTSModel = KokoroTTSModel()
+    @State private var sayThis : String = "Hello Everybody"
+    @State private var status : String = ""
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "mouth")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("TTS Examples")
+                .font(.headline)
+                .padding()
+            
+            TextField("Enter text", text: $sayThis)
+            
+            Button("Kokoro") {
+                Task {
+                    status = "Generating..."
+                    await kokoroTTSModel.say(sayThis, .bmGeorge)
+                    status = "Done"
+                }
+            }
+            
+            Text(status)
+                .font(.caption)
+                .padding()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
