c5ece67e5b68487bdf761679749a0f2d44de8f50
BenLumenDigital <53438826+BenLumenDigital@users.noreply.github.com>
2025-05-22 09:45:04 +0100

    Pretty up UI a bit

--- mlx_audio_swift/tts/Swift-TTS/ContentView.swift
+++ mlx_audio_swift/tts/Swift-TTS/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
 
     @State private var sayThis : String = "Hello Everybody"
     @State private var status : String = ""
+    
+    private var availableProviders = ["kokoro", "orpheus"]
+    @State private var chosenProvider : String = "kokoro"
 
     var body: some View {
         VStack {
@@ -23,30 +26,48 @@ struct ContentView: View {
             Text("TTS Examples")
                 .font(.headline)
                 .padding()
+                        
+            Picker("Choose a provider", selection: $chosenProvider) {
+                ForEach(availableProviders, id: \.self) { provider in
+                    Text(provider.capitalized)
+                }
+            }
+            .onChange(of: chosenProvider) { newProvider in
+                if newProvider == "orpheus" {
+                    status = "Orpheus is currently quite slow (0.05x on M1).  Working on it!\n\nBut it does support expressions: <laugh>, <chuckle>, <sigh>, <cough>, <sniffle>, <groan>, <yawn>, <gasp>"
+                } else {
+                    status = ""
+                }
+            }
+            .padding()
 
             TextField("Enter text", text: $sayThis)
 
-            Button("Kokoro") {
+            
+            Button(action: {
                 Task {
                     status = "Generating..."
-                    if kokoroTTSModel == nil {
-                        kokoroTTSModel = KokoroTTSModel()
+                    if chosenProvider == "kokoro" {
+                        if kokoroTTSModel == nil {
+                            kokoroTTSModel = KokoroTTSModel()
+                        }
+                        await kokoroTTSModel!.say(sayThis, .bmGeorge)
+                        
+                    } else if chosenProvider == "orpheus" {
+                        if orpheusTTSModel == nil {
+                            orpheusTTSModel = OrpheusTTSModel()
+                        }
+                        await orpheusTTSModel!.say(sayThis, .tara)
                     }
-                    await kokoroTTSModel!.say(sayThis, .bmGeorge)
+                    
                     status = "Done"
                 }
-            }
-
-            Button("Orpheus") {
-                Task {
-                    status = "Generating..."
-                    if orpheusTTSModel == nil {
-                        orpheusTTSModel = OrpheusTTSModel()
-                    }
-                    await orpheusTTSModel!.say(sayThis, .tara)
-                    status = "Done"
-                }
-            }
+            }, label: {
+                Text("Generate")
+                    .font(.title2)
+                    .padding()
+            })
+            .buttonStyle(.borderedProminent)
 
             Text(status)
                 .font(.caption)
