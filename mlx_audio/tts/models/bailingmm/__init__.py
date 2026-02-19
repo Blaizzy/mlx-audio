48a7c5e30eb3e3c0f1cadfeadf47a56d40525c18
Prince Canuma <prince.gdt@gmail.com>
2026-02-19 13:38:10 +0100

    rename to bailingmm

--- mlx_audio/tts/models/bailingmm/__init__.py
+++ mlx_audio/tts/models/bailingmm/__init__.py
@@ -1,4 +1,4 @@
 from .convert import convert_campplus_onnx_to_safetensors
-from .ming_omni_tts import Model, ModelConfig
+from .bailingmm import Model, ModelConfig
 
 __all__ = ["Model", "ModelConfig", "convert_campplus_onnx_to_safetensors"]
